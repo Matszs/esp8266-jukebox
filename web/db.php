dc40f85b54038030ba6a1da2b7665e3fb14ef3a3
Patrick Hendriks <p.hendriks1990@gmail.com>
2017-06-13 13:17:48 +0200

    File upload and convert added and working. Makes entry in db to show in json list

--- web/db.php
+++ web/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$db = new PDO('sqlite:sounds.db');
+$db = new PDO('sqlite:/var/www/esp8266-jukebox/web/db/sounds.db');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 ?>
