903c91c0087b9ea065a2ea1d4b2bac385d32f32b
Mats Otten <mats@kepsi.nl>
2017-06-17 17:32:03 +0200

    encoder ++/-- 1, RISING instead of CHANGE

--- esp8266-jukebox/esp8266-jukebox.ino
+++ esp8266-jukebox/esp8266-jukebox.ino
@@ -38,10 +38,17 @@ void pinTrigger() {
   int encoded = (MSB << 1) | LSB; // converting the 2 pin value to single number
   int sum  = (lastEncoded << 2) | encoded; // adding it to the previous encoded value
 
-  if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
+  Serial.println(sum, BIN);
+
+  /*if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
     encoderValue ++;
   if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
-    encoderValue --;
+    encoderValue --;*/
+
+    if(sum == 0b1101)
+      encoderValue++;
+    else if(sum == 0b1110)
+      encoderValue--;
 
    lastEncoded = encoded; //store this value for next time
 
@@ -95,7 +102,7 @@ void setup() {
   
   attachInterrupt(digitalPinToInterrupt(encoderPin1), pinTrigger, CHANGE);
   attachInterrupt(digitalPinToInterrupt(encoderPin2), pinTrigger, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(selectButton), selectButtonTrigger, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(selectButton), selectButtonTrigger, RISING);
 
   setupDisplay();
 }
